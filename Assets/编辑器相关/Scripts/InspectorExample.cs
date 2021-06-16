af0f5bd7343432ec224c5b433de1f2743be00917
Honhonglin <49089733+Honghonglin@users.noreply.github.com>
2021-06-17 00:19:59 +0800

    自定义Inspector面板

---
+++ Assets/编辑器相关/Scripts/InspectorExample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Course
+{
+    Chinese,
+    Mathematics,
+    English
+}
+
+
+public class InspectorExample : MonoBehaviour
+{
+    public int intValue;
+    public float floatValue;
+    public string stringValue;
+    public bool boolValue;
+    public Vector3 vector3Value;
+    public Course enumValue = Course.Chinese;
+    public Color colorValue = Color.white;
+    public Texture textureValue;
+}
